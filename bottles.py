040d9f2ed44f067eddd58745c782d47690d382f2
bitterguy-code <jasonb4cake@gmail.com>
2025-02-06 15:11:33 -0500

    running 99 to 0

--- bottles.py
+++ bottles.py
@@ -1,6 +1,11 @@
+# 99 bottles of beer on the wall, 99 bottles of beer.
+# Take one down and pass it around, 98 bottles of beer on the wall.
+# 98 bottles of beer on the wall, 98 bottles of beer.
+
 def bottle_song():
-	# write your code here!
-	# test
+	for i in range(99,0,-1):
+		print(str(i) + " bottles of beer on the wall, " + str(i) + " bottles of beer.")
+		print("Take on down and pass it around, " + str(i-1) + " bottles of beer on the wall.")
 	pass
 
 bottle_song()
